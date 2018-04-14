4ea5ac5d8ef7776e437a150e607f64b3cdc5cd4d
Vasilena Chobanova <vasilenach@hotmail.com>
2018-04-14 19:31:28 +0200

    access personal account

--- routes/account.js
+++ routes/account.js
@@ -126,7 +126,32 @@ router.put("/:userNo", function(req,res,next){
             }  
         }); 
     }
-   
+});
 
+// Access personal account
+router.get("/", function(req,res,next){
+
+    var jSession = JSON.parse(global.gSession);
+    if(jSession == null){
+        res.status(403);
+        res.send(JSON.stringify({response: "You need to be logged in!"}));
+    }
+    else if (jSession != null && jSession.isLoggedIn == true){
+        var userNo = jSession.userName;
+        var sQuery = "SELECT userNo, name, address, phone, email, userName, image from user WHERE userName = ?";
+
+        dbController.query(sQuery, [userNo], (err, sjData) => {
+            if(err){
+                console.log(err);
+                return res.send(JSON.stringify(err));
+            }
+            console.log(sjData);
+            return res.send(sjData);
+        });
+    }
+    else {
+        res.status(401);
+        res.send(JSON.stringify({response: "Unauthorized access!"}));
+    }
 });
 module.exports = router;
