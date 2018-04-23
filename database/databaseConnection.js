b9a8c4fe22c47a0dc50f10aa37c1e1be4e34b41e
Vasilena Chobanova <vasilenach@hotmail.com>
2018-04-23 10:43:21 +0200

    database connection small adjustment

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -3,6 +3,8 @@ global.gPool = null;
 
 const _pool = _mysql.createPool({
     host: 'localhost',
+    // user: 'sola',
+    // password: '951025',
     user: 'root',
     password: '',
     database: 'websec'
